e28d64f436c15503d06d3a0f71b135fa29eb082d
efcaguab <efcaguab@gmail.com>
2015-06-07 14:49:09 +1200

    use detections and acoustic_array objects

--- R/center-of-activity.R
+++ R/center-of-activity.R
@@ -1,24 +1,15 @@
 #' Calculate center of activity
 #' 
-#' @description
-#' Calculates the center of activity positions
-#' 
-#' @param station_names vector containing the station names (or receiver ids) 
-#'   per array
-#' @param station_positions numeric matrix, data.frame or SpatialPoints object 
-#'   with the coordinates (Longitude/Latitude) of the stations. Each row is a 
-#'   station and there must be only one coordinate per station
-#' @param det_stations vector with the names of the receiver id/stations name 
-#'   where the tag was detected. All receiver ids/station names in this vector 
-#'   must have been defined in the station_names argument
-#' @param det_times vector containing the times of the detections. If the vector
-#'   is not an Date-Time (POSIXct) object, it would be coerced to POSIXct using 
-#'   \code{\link[readr]{parse_datetime}}
-#' @param breaks a vector of cut points or number giving the number of 
-#'   intervals which the detections are to be grouped OR an interval 
-#'   specification. Intervals can be one of "sec", "min", "hour", "day", 
-#'   "DSTday", "week", "month", "quarter" or "year", optionally preceded by an 
-#'   integer and a space, or followed by "s"
+#' @description Calculates the center of activity in an observation network.
+#'   
+#' @param detections an object of the class 'detections' created with the function \code{\link{detections}}
+#' @param acoustic_array an object of the class 'acoustic_array' created with the function \code{\link{acoustic_array}}
+
+#' @param breaks a vector of cut points or number giving the number of intervals
+#'   which the detections are to be grouped OR an interval specification. 
+#'   Intervals can be one of "sec", "min", "hour", "day", "DSTday", "week", 
+#'   "month", "quarter" or "year", optionally preceded by an integer and a 
+#'   space, or followed by "s"
 #' @param method "observation-weighted", "model-weighted" or average. If 
 #'   "model-weighted" is selected, a logistic function must be specified in the 
 #'   model argument. See details section for more information
@@ -29,77 +20,113 @@
 #'   fields P0 (detection probability at a distance 0), D50 (distance 
 #'   corresponding to a detection probability of 50\%), and D95 (distance 
 #'   corresponding to a detection probability of 5\%)
+#' @param det_lon,det_lat numeric vectors containing the longitude of each
+#'   detection specified in 'det_stations' and 'det_times'
+#' @param ... other arguments passed to \code{coa}
 #'   
 #' @return 
-#' None  
+#' A Data frame
 #' @details 
-#' Three methods were evaluated for estimating a tag’s position at time
-#' t when detected by two or more receivers during a time interval (deltat). The 
-#' first method (termed ‘Average’) was to compute the mean position (latitude, 
-#' longitude) of the detecting receivers. The second was [7] method of using the
-#' mean of receiver locations weighted by the number of detections 
-#' (‘Observation-Weighted’, Table 6). The third modified the 
-#' ‘Observation-Weighted’ method to incorporate regression model estimates of 
-#' tag distance from a receiver (‘Model-Weighted’, Table 6). The weighting term
-#' was computed as the ratio of estimated distance between the tag and detecting
-#' receiver i (d i ) relative to the farthest estimated distance of the tag from
-#' any of the detecting receivers (d max ). The estimated distance (d i ) is
-#' determined by inputting the observed detection rate into the logistic
-#' function. Following [7], both arithmetic and harmonic mean estimators for
-#' each method were evaluated.
 #' 
-#' @references
-#' Farmer, N. a, Ault, J. S., Smith, S. G., & Franklin, E. C. (2013). Methods for assessment of short-term coral reef fish movements within an acoustic array. Movement Ecology, 1(1), 7. \url{http://doi.org/10.1186/2051-3933-1-7}
-#' 
-#' Simpfendorfer, C., & Heupel, M. (2002). Estimation of short-term centers of activity from an array of omnidirectional hydrophones and its use in studying animal movements. Canadian Journal of Fisheries and Aquatic Sciences, 32, 23–32. \url{http://doi.org/10.1139/F01-191}
-#' 
+#' Three methods for center of activity are providedwere evaluated for estimating a tag’s position at time
+#'   t when detected by two or more receivers during a time interval (deltat).
+#'   The first method (termed ‘Average’) was to compute the mean position
+#'   (latitude, longitude) of the detecting receivers. The second was [7] method
+#'   of using the mean of receiver locations weighted by the number of
+#'   detections (‘Observation-Weighted’, Table 6). The third modified the 
+#'   ‘Observation-Weighted’ method to incorporate regression model estimates of 
+#'   tag distance from a receiver (‘Model-Weighted’, Table 6). The weighting
+#'   term was computed as the ratio of estimated distance between the tag and
+#'   detecting receiver i (d i ) relative to the farthest estimated distance of
+#'   the tag from any of the detecting receivers (d max ). The estimated
+#'   distance (d i ) is determined by inputting the observed detection rate into
+#'   the logistic function. Following [7], both arithmetic and harmonic mean
+#'   estimators for each method were evaluated.
+#'   
+#'   All receiver ids/station names in this vector 
+#'   must have been defined in the station_names argument
+#'   
+#' @references Farmer, N. a, Ault, J. S., Smith, S. G., & Franklin, E. C. 
+#'   (2013). Methods for assessment of short-term coral reef fish movements
+#'   within an acoustic array. Movement Ecology, 1(1), 7. 
+#'   \url{http://doi.org/10.1186/2051-3933-1-7}
+#'   
+#'   Simpfendorfer, C., & Heupel, M. (2002). Estimation of short-term centers of
+#'   activity from an array of omnidirectional hydrophones and its use in
+#'   studying animal movements. Canadian Journal of Fisheries and Aquatic
+#'   Sciences, 32, 23–32. \url{http://doi.org/10.1139/F01-191}
+#'   
 #' @examples
 #' plot_crayons()
 #' 
 #' @export
-# 
-
-coa <- function (station_names, 
-                 station_positions,
-                 det_stations,
-                 det_times,
+coa <- function (detections,
+                 acoustic_array,
                  breaks = "5 min", 
                  method = c("observation-weighted", "model-weighted", "average"),
                  mean_type = c("arithmetic", "harmonic"),
                  model = NULL) {
   
   # Get positions
-  positions <- get_positions(station_names, station_positions)
-  detections <- get_detections(det_stations, det_times)
+  stopifnot(inherits(detections, "detections"),
+            inherits(acoustic_array, "acoustic_array"))
+  
   
   # Check that every receiver in detections has a position
-  if (!all(detections$rec %in% positions$rec)) 
+  if (!all(detections$station %in% acoustic_array$station)) 
     stop("Not all station-names/receiver-ids in 'det_stations' are present in 'station_names'")
 
-  detections %<>% 
-    mutate(breaks = cut (time, breaks)) %>% # Cut by the given breaks
-    group_by(breaks, rec)
+  # load magrittr
+  require(magrittr)
   
-  if (method == "average"){
-    detections %<>% summarise(weight = 1) %>% group_by()
-  } else if (method == "observation-weighted") {
-    detections %<>% summarise(weight = n()) %>% group_by()
-  } else if (method == "model-weighted") {
+  detections %<>% 
+    dplyr::mutate(breaks = cut (time, breaks)) %>% # Cut by the given breaks
+    dplyr::group_by(breaks, station)
+  
+  if (method[1] == "average"){
+    detections %<>% dplyr::summarise(weight = 1) %>% dplyr::group_by()
+  } else if (method[1] == "observation-weighted") {
+    detections %<>% dplyr::summarise(weight = n()) %>% dplyr::group_by()
+  } else if (method[1] == "model-weighted") {
     detections %<>% 
-      summarise(det = n()) %>%
-      group_by() %>%
-      mutate(det_prob = det/max(det),
-             distance = dist_dp(det_prob, model)) %>%
-      group_by(breaks) %>%
-      mutate(max_dist = max(distance)) %>%
-      group_by() %>%
-      mutate(weight = max_dist / distance)
+      dplyr::summarise(det = n()) %>%
+      dplyr::group_by() %>%
+      dplyr::mutate(det_prob = det/max(det),
+                    distance = dist_dp(det_prob, model)) %>%
+      dplyr::group_by(breaks) %>%
+      dplyr::mutate(max_dist = max(distance)) %>%
+      dplyr::group_by() %>%
+      dplyr::mutate(weight = max_dist / distance)
   }
   
   detections %<>%
-    inner_join(positions) %>%
-    group_by(breaks) %>%
-    summarise(lon = vers_mean(lon, weight, mean_type),
-              lat = vers_mean(lat, weight, mean_type))
-    
+    dplyr::inner_join(acoustic_array, by = "station") %>%
+    dplyr::group_by(breaks) %>%
+    dplyr::summarise(lon = vers_mean(lon, weight, mean_type),
+                     lat = vers_mean(lat, weight, mean_type))
+  
+  structure(detections, class = c("data.frame", "coa", "position"))
+}
+
+#' @describeIn coa A wrapper for \code{coa()} in which there is one longitude/latitude
+#'   pair for each detection
+#' @export
+coa_d <- function(det_stations, det_times, det_lon, det_lat, ...) {
+  
+  if(!zero_range(c(length(det_stations),
+                  length(det_times),
+                  length(det_lon),
+                  length(det_lat)))) stop("'det_stations', 'det_times', 'det_lon', and 'det_lat' must have the same length")
+  
+  # Join in a data frame
+  det <- data.frame (name = det_stations, 
+                     time = det_times, 
+                     lon = det_lon, 
+                     lat = det_lat)
+  
+  # Get the location of the stations
+  stations <- det %>% select (name, lon, lat) %>% distinct ()
+  
+  # Calculate the coa
+  coa(det$name, det$time, stations$name, stations %>% select(lon, lat), ...)
 }
--- R/miscelaneous_functions.R
+++ R/miscelaneous_functions.R
@@ -1,83 +1,47 @@
-get_positions <- function (station_names, station_positions) {
-  # Get positions in a standard way given possible input formats, and check that
-  # everything is correct
-  
-  # Standarise station positions
-  station_positions <- std_positions(station_positions) 
-  # Check that there is only one location per receiver 
-  if(length(station_names) != nrow(station_positions))
-    stop ("Number of stations in 'station_names' differ to the number of coordinates in 'station_positions'")
-  
-  positions <- data_frame (rec = station_names) %>%
-    bind_cols(station_positions)
-  
-  # If there are NA's
-  if(positions %>% 
-       lapply(function(x) any(is.na(x))) %>%
-       unlist() %>% any()) stop("One or more of the coordinates or station names is missing or is not a valid value")
-  
-  return(positions)
-}
+# get_positions <- function (station_names, station_positions) {
+#   # Get positions in a standard way given possible input formats, and check that
+#   # everything is correct
+#   
+#   # Standarise station positions
+#   station_positions <- std_positions(station_positions) 
+#   # Check that there is only one location per receiver 
+#   if(length(station_names) != nrow(station_positions))
+#     stop ("Number of stations in 'station_names' differ to the number of coordinates in 'station_positions'")
+#   
+#   positions <- data_frame (rec = station_names) %>%
+#     bind_cols(station_positions)
+#   
+#   # If there are NA's
+#   if(positions %>% 
+#        lapply(function(x) any(is.na(x))) %>%
+#        unlist() %>% any()) stop("One or more of the coordinates or station names is missing or is not a valid value")
+#   
+#   return(positions)
+# }
+# 
+# 
 
 
-std_positions <- function (station_positions) {
-  # Transform any of the possible station positions into a data frame with lat
-  # and lon
-  
-  # If it's a tbl_df convert to a normal data.frame
-  if(any(class(station_positions) == "data.frame")) 
-    class(station_positions) <- "data.frame"
-  
-  # If it's a SpacialPoints object extract the coordinates
-  if(any(class(station_positions) == "SpatialPoints"))
-    station_positions <- coordinates(station_positions)
-  
-  # Check that it has at least two columns
-  if(ncol(station_positions) < 2) stop("'station_positions' has less than two columns")
-  
-  # Check that there are columns called latitude/longitude
-  if(all(c("lat", "lon") %in% colnames(station_positions))){
-    out_pos <- data_frame(lon = station_positions[,"lon"],
-                          lat = station_positions[, "lat"])
-  } else if(all(c("latitude", "longitude") %in% colnames(station_positions))){
-    out_pos <- data_frame(lon = station_positions[,"longitude"],
-                          lat = station_positions[, "latitude"])
-  } else {
-    if(ncol(station_positions) > 2) 
-      warning("'station_positions' has more than two columns, only the first two are used")
-    out_pos <- data_frame(lon = station_positions[, 1],
-                          lat = station_positions[, 2])
-  }
-  
-  # Coerce to numeric
-  if(!(is.numeric(out_pos$lat) & is.numeric(out_pos$lon))) {
-    warning("Coordinates are not numbers, coercing to 'numeric'")
-    out_pos %<>% mutate(lat = as.numeric(lat), lon = as.numeric(lon))
-  }
-  
-  return(out_pos)
-}
 
-
-get_detections <- function (det_stations, det_times) {
-  # Get detections in a standard way. Check that dates and formats are correct
-  
-  # Check that times are correct
-  if(!any(class(det_times) == "POSIXct")) {
-    warning("'det_times' is not a POSIXct object. Coervertion will at UTC timezone will be attempted")
-    det_times <- parse_datetime(det_times)
-    if(any(is.na(det_times))) warning("Problems parsing date-times detected")
-  }
-  
-  detections <- data_frame(rec = det_stations, time = det_times)
-  
-  # Check if there is NA's
-  if(detections %>% lapply(function(x) any(is.na(x))) %>% unlist() %>% any())
-    warning("Missing or invalid values detected in 'det_stations' or 'det_times'. NA's will be removed" )
-  
-  detections %<>% 
-    filter(!is.na(rec), !is.na(time))
-}
+# get_detections <- function (det_stations, det_times) {
+#   # Get detections in a standard way. Check that dates and formats are correct
+#   
+#   # Check that times are correct
+#   if(!any(class(det_times) == "POSIXct")) {
+#     warning("'det_times' is not a POSIXct object. Convertion with UTC timezone will be attempted")
+#     det_times <- readr::parse_datetime(det_times)
+#     if(any(is.na(det_times))) warning("Problems parsing date-times detected")
+#   }
+#   
+#   detections <- data_frame(rec = det_stations, time = det_times)
+#   
+#   # Check if there is NA's
+#   if(detections %>% lapply(function(x) any(is.na(x))) %>% unlist() %>% any())
+#     warning("Missing or invalid values detected in 'det_stations' or 'det_times'. NA's will be removed" )
+#   
+#   detections %<>% 
+#     filter(!is.na(rec), !is.na(time))
+# }
 
 std_model <- function(model) {
   
@@ -120,12 +84,12 @@ std_model <- function(model) {
 vers_mean <- function(x, w, mean_type = "arithmetic") {
   # Calculates weighted arithmetic or harmonic means
 
-  if(mean_type == "harmonic") {
+  if(mean_type[1] == "harmonic") {
     y <- 1 / weighted.mean(1 / x, w)
   } else {
     y <- weighted.mean (x, w)
   } 
-  return(y)
+  y
 }
 
 dist_dp <- function(det_prob, model) {
@@ -141,4 +105,11 @@ dist_dp <- function(det_prob, model) {
   dist <- (logit(det_prob) - co[1]) / co[2]  # Invert the formula
   dist[dist < 1] <- 1  # Only return positive distances...
   return(dist)
+}
+
+# Determine if all values in a vector are the same
+zero_range <- function(x, tol = .Machine$double.eps ^ 0.5) {
+  if (length(x) == 1) return(TRUE)
+  x <- range(x) / mean(x)
+  isTRUE(all.equal(x[1], x[2], tolerance = tol))
 }
