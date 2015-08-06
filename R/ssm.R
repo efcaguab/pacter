ac183dd9e7163a4abdf1871eefc12fe122ccc82e
efcaguab <efcaguab@gmail.com>
2015-08-06 16:41:28 +1200

    add option to change parameters in the tpl file

--- R/ssm.R
+++ R/ssm.R
@@ -16,6 +16,7 @@ ssm <- function(detections,
                 n_cells = 2500,
                 start_time = NULL,
                 end_time = NULL,
+                params = list(logbx = -9, logby = -9, logitcor = 0, logsigma = 0.4),
                 safe=FALSE,
                 verbose=FALSE,
                 admb_errors=c("stop","warn","ignore")){
@@ -42,21 +43,51 @@ ssm <- function(detections,
                                start_time = NULL, end_time = NULL)
   write_ssm_data(state_space_data)
   
-
-  # cf it has never been compiled, do so
-  if(system.file("onssm", package= "pacter") == ""){
-    setwd(system.file(package = "pacter"))
+  default_params <- identical(params, list(logbx = -9, logby = -9, logitcor = 0, logsigma = 0.4))
+  
+  if (default_params){
+    
+    # cf it has never been compiled, do so
+    if(system.file("onssm", package= "pacter") == ""){
+      setwd(system.file(package = "pacter"))
+      compile_admb("onssm", 
+                   safe = safe, 
+                   re = FALSE,
+                   verbose = verbose,
+                   admb_errors = admb_errors)
+    } 
+    
+    # copy the compiled software into the temporary directory
+    file.copy(system.file("onssm", package= "pacter"), tempdir())
+    file.copy(system.file("onssm.tpl", package= "pacter"), tempdir())
+    file.create(file.path(tempdir(), "onssm.dat"))
+  } else {
+    # copy the tpl
+    file.copy(system.file("onssm.tpl", package= "pacter"), tempdir())
+    file.create(file.path(tempdir(), "onssm.dat"))
+    # modify the tpl
+    the_tpl <- readLines(file.path(tempdir(), "onssm.tpl"))
+    logbx <- paste("  logbx = ", params$logbx, ";", sep ="")
+    logby <- paste("  logby = ", params$logby, ";", sep ="")
+    logitcor <- paste("  logitcor = ", params$logitcor, ";", sep ="")
+    logsigma <- paste("  logsigma = ", params$logsigma, ";", sep ="")
+    the_tpl[352] <- logbx
+    the_tpl[353] <- logby
+    the_tpl[354] <- logitcor
+    the_tpl[355] <- logsigma
+    writeLines(the_tpl, file.path(tempdir(), "onssm.tpl"))
+    # compile it
+    # make the temporary directory the working directory while executing
+    setwd(tempdir())
     compile_admb("onssm", 
                  safe = safe, 
                  re = FALSE,
                  verbose = verbose,
                  admb_errors = admb_errors)
-  } 
+  }
+
   
-  # copy the compiled softwar into the temporary directory
-  file.copy(system.file("onssm", package= "pacter"), tempdir())
-  file.copy(system.file("onssm.tpl", package= "pacter"), tempdir())
-  file.create(file.path(tempdir(), "onssm.dat"))
+  
   
   # make the temporary directory the working directory while executing
   setwd(tempdir())
